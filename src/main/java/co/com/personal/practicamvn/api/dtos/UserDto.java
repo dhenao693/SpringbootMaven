9b75e3ada8c0dea003f7b19c3f1d793daf6938ba
Daniel Henao <dhenao693@gmail.com>
2024-02-15 10:44:06 -0500

    iniciar pproyecto y agregar swagger UI

---
+++ src/main/java/co/com/personal/practicamvn/api/dtos/UserDto.java
@@ -0,0 +1,19 @@
+package co.com.personal.practicamvn.api.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private long id;
+    private UserInfoContactDTO idUserInfo;
+    private String password;
+    private String user;
+    private String userCreate;
+    private String dateCreate;
+}
